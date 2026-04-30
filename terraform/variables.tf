d017ff87a35df72049ff3045962d7cca366bb714
Andreas Paschwitz <842659+paschi@users.noreply.github.com>
2026-04-30 17:47:22 +0200

    feat(terraform): include May and June in poll schedule expressions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,7 +23,7 @@ variable "start_poll_message" {
 
 variable "start_poll_schedule_expression" {
   type    = string
-  default = "cron(0 20 15 1,2,3,4,7,8,9,10,12 ? *)"
+  default = "cron(0 20 15 1,2,3,4,5,6,7,8,9,10,12 ? *)"
 }
 
 variable "start_poll_schedule_expression_december" {
@@ -63,7 +63,7 @@ variable "end_poll_message" {
 
 variable "end_poll_schedule_expression" {
   type    = string
-  default = "cron(0 13 L 1,2,3,4,7,8,9,10,11,12 ? *)"
+  default = "cron(0 13 L 1,2,3,4,5,6,7,8,9,10,11,12 ? *)"
 }
 
 variable "end_poll_schedule_flexible_time_window_mode" {
